b0059fe27ad766c11e605e56a5fed3965d1c58d4
Michelle Olivieri <michelleo72@gmail.com>
2018-05-01 22:32:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,12 +11,12 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/cart/)
-      @@cart.each do |c|
-        resp.write "#{c}\n"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |c|
+        resp.write "#{c}\n"
     else
       resp.write "Path Not Found"
     end
